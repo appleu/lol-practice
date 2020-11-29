e1f3e4005930f3180e9787e61a40c02ad5eb0679
X-zhihua <uapplem@sohu.com>
2020-11-29 22:59:43 +0800

    popattend-：洗完澡再写

---
+++ js/pop.js
@@ -0,0 +1,24 @@
+
+   function showPop(id){
+    //    console.log(1);
+        $('#'+id).style.display = 'flex';
+   } 
+
+   let array = $$('.pop_close');
+   for (let i = 0; i < array.length; i++) {
+       const element = array[i];
+       element.onclick = function(){
+           let el = this.parentElement.parentElement;
+            el.style.display="none";           
+       }
+   }
+
+$('.pop_wechat').onclick = function(){
+    this.classList.add('selected');
+    $('.pop_qq').classList.remove('selected');
+}
+
+$('.pop_qq').onclick = function(){
+    this.classList.add('selected');
+    $('.pop_wechat').classList.remove('selected');
+}
